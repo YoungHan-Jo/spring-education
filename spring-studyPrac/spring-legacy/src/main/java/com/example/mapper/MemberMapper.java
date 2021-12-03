e3102bdc3fb4e66aacdd14dec69b559271ef35ef
JYH <verin4494@gmail.com>
2021-12-03 19:11:04 +0900

    1203

--- spring-studyPrac/spring-legacy/src/main/java/com/example/mapper/MemberMapper.java
+++ spring-studyPrac/spring-legacy/src/main/java/com/example/mapper/MemberMapper.java
@@ -4,6 +4,10 @@ import com.example.domain.MemberVO;
 
 public interface MemberMapper {
 
+	// 회원가입
 	void addMember(MemberVO memberVO);
+	
+	// db에 해당하는 아이디 개수 받아오기
+	int getCountById(String id);
 	
 }
--- spring-studyPrac/spring-legacy/src/main/java/com/example/service/MemberService.java
+++ spring-studyPrac/spring-legacy/src/main/java/com/example/service/MemberService.java
@@ -17,6 +17,10 @@ public class MemberService {
 		memberMapper.addMember(memberVO);
 	}
 	
+	public int getCountById(String id) {
+		
+		return memberMapper.getCountById(id);
+	}
 	
 	
 }
